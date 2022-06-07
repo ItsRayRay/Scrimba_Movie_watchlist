c71b6eb6ac80334437619772a007b026f10f309a
ItsRayRay <r.boerema21@gmail.com>
2022-06-07 12:24:52 +0200

    fix layout issue pushing movies to top and icon to center main with display flex

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const cardList = document.querySelector("main")
 
 
 
-cardList.innerHTML = `<div id="center__page">
+cardList.innerHTML = `<main style ="display:flex" ><div id="center__page">
 <img src="Icon.movie.png" alt="">
 <div><h4>Start exploring</h4></div>
-</div>
+</div></main>
 `
+
 
 searchButton.addEventListener("click", function(e) { 
 e.preventDefault()
@@ -18,16 +19,14 @@ let searchInput = document.querySelector("input").value
 cardList.innerHTML = ""
 
 cardList.innerHTML = `<div class="card">
-
 <div><img src="bladerunner.png" alt=""></div>
-
 <div>
 <div class="title">Blade Runner <span class="star"> ⭐ </span> <span class="rating">8.1</span></div>
 <div class="movie__info"><span>117 min</span><span>Action, Drama, Sci-fi</span><div><span class="watchlist__icon"><img class="plusicon" src="plusicon.png" alt="">  Watchlist</span></div></div>
 <div class="movie__description">A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</div>
 </div>
-
 </div>
+
 `
 
 
@@ -46,6 +45,3 @@ fetch(`http://www.omdbapi.com/?t=${searchInput}?&apikey=d45d22f1`)
 
 
 })
-
-
-
