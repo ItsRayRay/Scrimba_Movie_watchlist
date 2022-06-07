35537f20b48ece184d748dedf93afe304ae75318
ItsRayRay <r.boerema21@gmail.com>
2022-06-07 12:00:06 +0200

    add card to dom element

--- script.js
+++ script.js
@@ -3,10 +3,40 @@ const watchListButton = document.querySelector("#watchlist__btn")
 const cardList = document.querySelector("main")
 
 
+
+cardList.innerHTML = `<div id="center__page">
+<img src="Icon.movie.png" alt="">
+<div><h4>Start exploring</h4></div>
+</div>
+`
+
 searchButton.addEventListener("click", function(e) { 
 e.preventDefault()
 
 let searchInput = document.querySelector("input").value
+
+cardList.innerHTML = ""
+
+cardList.innerHTML = `<div class="card">
+
+<div><img src="bladerunner.png" alt=""></div>
+
+<div>
+<div class="title">Blade Runner <span class="star"> ⭐ </span> <span class="rating">8.1</span></div>
+<div class="movie__info"><span>117 min</span><span>Action, Drama, Sci-fi</span><div><span class="watchlist__icon"><img class="plusicon" src="plusicon.png" alt="">  Watchlist</span></div></div>
+<div class="movie__description">A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</div>
+</div>
+
+</div>
+`
+
+
+
+
+
+
+
+
 
 fetch(`http://www.omdbapi.com/?t=${searchInput}?&apikey=d45d22f1`)
 .then(response => response.json())
@@ -15,5 +45,7 @@ fetch(`http://www.omdbapi.com/?t=${searchInput}?&apikey=d45d22f1`)
 
 
 
-
 })
+
+
+
