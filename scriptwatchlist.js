9f6cce29789b1cfc6fe6c2535ae74e896bea7740
ItsRayRay <r.boerema21@gmail.com>
2022-06-08 19:01:25 +0200

    render card with info no style yet

--- scriptwatchlist.js
+++ scriptwatchlist.js
@@ -1,5 +1,6 @@
 const cardList = document.querySelector("main")  
 
+cardList.innerHTML = ""    
 
 for (let i = 0; i < localStorage.length; i++) {
 
@@ -7,13 +8,12 @@ let savedMovies = JSON.parse(localStorage.getItem(i))
 console.log(savedMovies)
 
 
-
 cardList.innerHTML += `<div class="card">
-<div><img class="movie__poster" src=" " alt=""></div>
+<div><img class="movie__poster" src="${savedMovies.poster} " alt=""></div>
 <div>
-<div class="title">  <span class="star"> ⭐ </span> <span class="rating"> </span></div>
-<div class="movie__info"><span></span><span></span><div><span class="watchlist__icon"><img onclick="saveMovie () "  class="plusicon" src="plusicon.png" alt="">  Watchlist</span></div></div>
-<div class="movie__description"> </div>
+<div class="title">${savedMovies.title}  <span class="star"> ⭐ </span> <span class="rating"> ${savedMovies.rating}</span></div>
+<div class="movie__info"><span>${savedMovies.runtime}</span><span>${savedMovies.genre}</span><div><span class="watchlist__icon"><img onclick="saveMovie () "  class="plusicon" src="plusicon.png" alt="">  Watchlist</span></div></div>
+<div class="movie__description"> ${savedMovies.plot}</div>
 </div>
 </div>
 `
