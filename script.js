795e7fe4a513ecc157cf2001c0b5b5c5e3d70b66
ItsRayRay <r.boerema21@gmail.com>
2022-06-08 13:17:30 +0200

    change fetch to async to make double api call

--- script.js
+++ script.js
@@ -2,11 +2,7 @@
 const searchButton = document.querySelector("#search__btn")
 const watchListButton = document.querySelector("#watchlist__btn")
 const cardList = document.querySelector("main")  
-let firstMovie = ""
-let secondMovie = ""
-let thirdMovie = ""
-let fourthMovie = ""
-let fifthMovie = ""
+let movieList = []
 
 
 cardList.innerHTML = `<main style ="display:flex" ><div id="center__page">
@@ -15,45 +11,36 @@ cardList.innerHTML = `<main style ="display:flex" ><div id="center__page">
 </div></main>
 `
 
+
+
+
 searchButton.addEventListener("click", function(e) {  //search button
-e.preventDefault()
-let searchInput = document.querySelector("input").value  // get the value of the input
+   e.preventDefault()
+   let searchInput = document.querySelector("input").value  // get the value of the input
+   
+   cardList.innerHTML = ""         // clears the cardList
+   
+async function getMovies() { 
+   const movieID = await fetch(`https://www.omdbapi.com/?s=${searchInput}&apikey=d45d22f1`)
+   const movieData = await movieID.json()
 
-let movieList = []
+  
+ const firstMovie =  movieData.Search[0].imdbID
+  const secondMovie =    movieData.Search[1].imdbID
+    const thirdMovie =  movieData.Search[2].imdbID
 
-cardList.innerHTML = ""         // clears the cardList
+ 
+ const firstMovieData = await fetch(`https://www.omdbapi.com/?i=${firstMovie}&apikey=d45d22f1`)
+ const firstMovieDataJson = await firstMovieData.json()
 
-
-fetch(`http://www.omdbapi.com/?s=${searchInput}?&apikey=d45d22f1`)      // fetching the data from the API
-.then(response => response.json())
-.then( movieDB =>     {  
-                         for (let i = 0; i < movieDB.Search.length; i++) {
-                            movieList.push(movieDB.Search[i].imdbID)  //loops trough the movieDB and adds the imdbID to the movieList
-                         } }).catch(error => console.log(error)) 
-
-
-
-
-console.log(typeof(movieList))
-
-for (let i = 0; i < movieList.length; i++) {
-                         
-fetch(`http://www.omdbapi.com/?i=tt3826198&apikey=d45d22f1`)
-.then(response => response.json())
-.then( movies=> {console.log(movies)} )
-
-
-
-
+ console.log(firstMovieDataJson)
 
 
 
 }
 
-} )
 
+getMovies() 
+.catch(err => console.log(err))
 
-
-
-
-
+})
