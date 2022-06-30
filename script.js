fed0d2ac319ae78bc9dc654ac08091e4e7715b8b
ItsRayRay <r.boerema21@gmail.com>
2022-06-30 10:47:57 +0200

    clean up code make more readable

--- script.js
+++ script.js
@@ -1,79 +1,111 @@
-
-const searchButton = document.querySelector("#search__btn")
-const watchListButton = document.querySelector("#watchlist__btn")
-const cardList = document.querySelector("main")  
-const addToWatchlist = document.querySelector(".watchlist__icon")
-let savedMovies = []
-let savedMovieObj = {}
+const searchButton = document.querySelector("#search__btn");
+const watchListButton = document.querySelector("#watchlist__btn");
+const cardList = document.querySelector("main");
+const addToWatchlist = document.querySelector(".watchlist__icon");
 
 cardList.innerHTML = `<main style ="display:flex" ><div id="center__page">
 <img src="Icon.movie.png" alt="">
 <div><h4>Start exploring</h4></div>
-</div></main>`
+</div></main>`;
 
 
+searchButton.addEventListener("click", function (e) {
+  //search button
+  e.preventDefault();
 
-searchButton.addEventListener("click", function(e) {  //search button
-   e.preventDefault()
+  let searchInput = document.querySelector("input").value; // get the value of the input
 
-   let searchInput = document.querySelector("input").value  // get the value of the input
+  cardList.innerHTML = ""; // clears the cardList
+
+  async function getMovies() {
+    const movieID = await fetch(
+      `https://www.omdbapi.com/?s=${searchInput}&apikey=d45d22f1`
+    );
+    const movieData = await movieID.json();
+
+    const movieList = movieData.Search; // get the movie list
+    const movielistID = movieList.map((movie) => movie.imdbID); // map trough the array and get the imdbID
+
+    for (let i = 0; i < movielistID.length; i++) {
+      // the array loops trough the lenght of all the imdbID and put it in the fetch as a parameter to get the movie data
+      const firstMovieData = await fetch(
+        `https://www.omdbapi.com/?i=${movielistID[i]}&apikey=d45d22f1`
+      );
+      const firstMovieDataJson = await firstMovieData.json();
+
+      let uniqueID = Date.now() // random number to make the movie object unique
+
+
    
-   cardList.innerHTML = ""         // clears the cardList
+   const card = document.createElement("div");
+   card.classList.add("card");
+   const div = document.createElement("div");
+   div.classList.add("flexbox");
+   const moviePoster = document.createElement("img");
+   moviePoster.classList.add("movie__poster");
+   moviePoster.src = firstMovieDataJson.Poster;
+   const divTitle = document.createElement("div");
+   divTitle.classList.add("title");
+   divTitle.innerHTML = firstMovieDataJson.Title;
+   const spanStar = document.createElement("span");
+   spanStar.classList.add("star");
+   spanStar.innerHTML = "⭐";
+   const spanRating = document.createElement("span");
+   spanRating.classList.add("rating");
+   spanRating.innerHTML = firstMovieDataJson.Ratings[0].Value;
+   const movieInfoDiv = document.createElement("div");
+   movieInfoDiv.classList.add("movie__info");
+   const spanRuntime = document.createElement("span");
+   spanRuntime.classList.add("runtime");
+   spanRuntime.innerHTML = firstMovieDataJson.Runtime;
+   const spanGenre = document.createElement("span");
+   spanGenre.classList.add("genre");
+   spanGenre.innerHTML = firstMovieDataJson.Genre;
+   const spanWatchlistIcon = document.createElement("span");
+   spanWatchlistIcon.classList.add("watchlist__icon");
+
+
+   const addToWatchlistBtn = document.createElement("button");
+   addToWatchlistBtn.textContent = "Add to Watchlist";
+   addToWatchlistBtn.classList.add("addToWatchlistBtn");
+
+   addToWatchlistBtn.addEventListener("click", function () {
+       // add to watchlist button
+         savedMovieObj = {
+             title: firstMovieDataJson.Title,
+               poster: firstMovieDataJson.Poster,
+               rating: firstMovieDataJson.Ratings[0].Value,
+               runtime: firstMovieDataJson.Runtime,
+               genre: firstMovieDataJson.Genre,
+               plot: firstMovieDataJson.Plot,
+               imdbID: firstMovieDataJson.imdbID,
+               uniqueID: uniqueID
+         }
+         localStorage.setItem(uniqueID, JSON.stringify(savedMovieObj));
+     
+   })
    
-async function getMovies() { 
-   const movieID = await fetch(`https://www.omdbapi.com/?s=${searchInput}&apikey=d45d22f1`)
-   const movieData = await movieID.json()
 
-  
+   const movieDescription = document.createElement("div");
+   movieDescription.classList.add("movie__description");
+   movieDescription.innerHTML = firstMovieDataJson.Plot;
 
-const movieList = movieData.Search // get the movie list
-const movielistID = movieList.map(movie => movie.imdbID) // map trough the array and get the imdbID
-
-
-
-for (let i = 0; i < movielistID.length; i++) {    // the array loops trough the lenght of all the imdbID and put it in the fetch as a parameter to get the movie data
- const firstMovieData = await fetch(`https://www.omdbapi.com/?i=${movielistID[i]}&apikey=d45d22f1`)
- const firstMovieDataJson = await firstMovieData.json()
-
-
- cardList.innerHTML += `<div class="card">
- <div><img class="movie__poster" src="${firstMovieDataJson.Poster} " alt=""></div>
- <div>
- <div class="title"> ${firstMovieDataJson.Title} <span class="star"> ⭐ </span> <span class="rating">${firstMovieDataJson.Ratings[0].Value} </span></div>
- <div class="movie__info"><span>${firstMovieDataJson.Runtime} </span><span>${firstMovieDataJson.Genre} </span><div><span class="watchlist__icon"><img onclick="saveMovie (${i}) "  class="plusicon" src="plusicon.png" alt="">  Watchlist</span></div></div>
- <div class="movie__description">${firstMovieDataJson.Plot} </div>
- </div>
- </div>
- `
-
-   savedMovieObj = { 
-                     title: firstMovieDataJson.Title,
-                     poster: firstMovieDataJson.Poster,
-                     rating: firstMovieDataJson.Ratings[0].Value,
-                     runtime: firstMovieDataJson.Runtime,
-                     genre: firstMovieDataJson.Genre,
-                     plot: firstMovieDataJson.Plot,
-
-                    }
-
-   savedMovies.push(savedMovieObj)
-}
+   cardList.append(card);
+   card.append(div);
+   div.append(moviePoster);
+   div.append(divTitle);
+   divTitle.append(spanStar);
+   divTitle.append(spanRating);
+   div.append(movieInfoDiv);
+   movieInfoDiv.append(spanRuntime);
+   movieInfoDiv.append(spanGenre);
+   div.append(addToWatchlistBtn);
+   div.append(movieDescription);
 
 
-}
+    }
+  }
 
-getMovies() 
-.catch(err => console.log(err))
-
-})
-
-
-
-function saveMovie(pass) {
-   
- let savedMoviesSerialized= JSON.stringify( savedMovies[pass] )
- localStorage.setItem (pass, savedMoviesSerialized )
-
-
-}
+  getMovies().catch((err) => console.log(err));
+});
 
